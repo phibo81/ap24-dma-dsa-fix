77b8d831012d23428b2ebe77111cea180dca24cf
phibo81 <79551070+phibo81@users.noreply.github.com>
2024-02-21 17:25:51 +0100

    i

--- ap24_dma_dsa_fix.js
+++ ap24_dma_dsa_fix.js
@@ -22,7 +22,7 @@ jQuery(document).ready(function(){
 
     // If is Exception
     if (isException) {
-        console.log('Exception case');
+        // console.log('Exception case');
         addStringToWebsite();
     }
 
